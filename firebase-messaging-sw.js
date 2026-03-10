ba010843701caba885a9907e48c790e929dfd85d
Arkan223 <76471453+Arkan223@users.noreply.github.com>
2026-03-10 22:59:31 +0300

    Refactor Firebase messaging service worker setup

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,20 +2,18 @@ importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  messagingSenderId: "871697042781",
-  databaseURL: "https://arkanlab-99711-default-rtdb.firebaseio.com/"
+    apiKey: "m58OJf1mYVHGuO1SJa2QydGIbL0gFnjQ6babKb5ayME",
+    messagingSenderId: "871697042781",
+    projectId: "arkanlab-99711"
 });
 
 const messaging = firebase.messaging();
 
-// معالجة الإشعارات عندما تكون الخلفية مغلقة
 messaging.onBackgroundMessage((payload) => {
-  console.log('Received background message ', payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: payload.notification.icon || 'https://cdn-icons-png.flaticon.com/512/1041/1041916.png'
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: 'https://cdn-icons-png.flaticon.com/512/1041/1041916.png'
+    };
+    self.registration.showNotification(notificationTitle, notificationOptions);
 });
